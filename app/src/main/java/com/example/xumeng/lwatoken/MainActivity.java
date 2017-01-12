96019a9c66ee93678cfd981ccca5c29d3e0f7677
melissa <xumeng919@gmail.com>
2017-01-12 10:26:12 -0500

    .pcm file, record and play succesfully by using AudioRecord,login correctly

--- app/src/main/java/com/example/xumeng/lwatoken/MainActivity.java
+++ app/src/main/java/com/example/xumeng/lwatoken/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.xumeng.lwatoken;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,7 +14,9 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,9 +34,18 @@ import com.amazon.identity.auth.device.api.workflow.RequestContext;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
     private static final String Tag= MainActivity.class.getName();
 
     private RequestContext mRequestContext;
@@ -48,14 +62,18 @@ public class MainActivity extends Activity {
     private ProgressBar loginProgress;
     private boolean isLoggedIn;
 
-    private static final String LOG_TAG = "AudioRecordTest";
-    private static String mFileName = null;
-    private MediaRecorder mRecorder = null;
-    private MediaPlayer mPlayer = null;
-    boolean mStartRecording = true;
-    boolean mStartPlaying = true;
-    private Button recordButton;
-    private Button playButton;
+//    private static final String LOG_TAG = "AudioRecordTest";
+//    private static String mFileName = null;
+//    private MediaRecorder mRecorder = null;
+//    private MediaPlayer mPlayer = null;
+//    boolean mStartRecording = true;
+//    boolean mStartPlaying = true;
+//    private Button recordButton;
+//    private Button playButton;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +84,9 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
         initializeUI();
+        initView();
 
+/*
         recordButton = (Button) findViewById(R.id.recording);
         playButton = (Button) findViewById(R.id.playing);
         recordButton.setOnClickListener(new View.OnClickListener(){
@@ -93,6 +113,8 @@ public class MainActivity extends Activity {
                 mStartPlaying = !mStartPlaying;
             }
         });
+*/
+
     }
 
     private void initializeUI(){
@@ -207,18 +229,14 @@ public class MainActivity extends Activity {
                             getAccessToken();
                         }
                     });
-
                     /* The user is signed in */
 
                 }else{
-
                 }
             }
-
             @Override
             public void onError(AuthError authError) {
                     /* The user is not signed in */
-
             }
         });
     }
@@ -237,8 +255,9 @@ public class MainActivity extends Activity {
     private void setLoggedInState(){
         mLoginButton.setVisibility(Button.GONE);
         loggoutTextView.setVisibility(Button.VISIBLE);
-        recordButton.setVisibility(View.VISIBLE);
-        playButton.setVisibility(View.VISIBLE);
+//        recordButton.setVisibility(View.VISIBLE);
+//        playButton.setVisibility(View.VISIBLE);
+        lnlayout.setVisibility(LinearLayout.VISIBLE);
         isLoggedIn=true;
         setLoggingInState(false);
     }
@@ -249,8 +268,9 @@ public class MainActivity extends Activity {
     private void setLoggedOutState(){
         mLoginButton.setVisibility(Button.VISIBLE);
         loggoutTextView.setVisibility(Button.GONE);
-        recordButton.setVisibility(View.GONE);
-        playButton.setVisibility(View.GONE);
+        lnlayout.setVisibility(LinearLayout.GONE);
+//        recordButton.setVisibility(View.GONE);
+//        playButton.setVisibility(View.GONE);
         isLoggedIn=false;
         resetTextCotent();
 
@@ -263,23 +283,27 @@ public class MainActivity extends Activity {
      */
     private void setLoggingInState(final boolean logginIn){
         if(logginIn){
+
             mLoginButton.setVisibility(Button.GONE);
             loggoutTextView.setVisibility(Button.GONE);
 //            setLoggedInButtonVisibility(Button.GONE);
             loginProgress.setVisibility(ProgressBar.VISIBLE);
             textContent.setVisibility(TextView.GONE);
-            recordButton.setVisibility(View.GONE);
-            playButton.setVisibility(View.GONE);
+            lnlayout.setVisibility(LinearLayout.GONE);
+//            recordButton.setVisibility(View.GONE);
+//            playButton.setVisibility(View.GONE);
         }else{
             if(isLoggedIn){
                 loggoutTextView.setVisibility(Button.VISIBLE);
+                lnlayout.setVisibility(LinearLayout.VISIBLE);
             }else{
                 mLoginButton.setVisibility(Button.VISIBLE);
             }
             textContent.setVisibility(TextView.VISIBLE);
             loginProgress.setVisibility(ProgressBar.GONE);
-            recordButton.setVisibility(View.VISIBLE);
-            playButton.setVisibility(View.VISIBLE);
+//
+//            recordButton.setVisibility(View.VISIBLE);
+//            playButton.setVisibility(View.VISIBLE);
         }
     }
     public class TokenListener implements Listener<AuthorizeResult, AuthError> {
@@ -369,6 +393,181 @@ public class MainActivity extends Activity {
     }
 
 
+    public static final String TAG = "PCMSample";
+    //是否在录制
+    private boolean isRecording = false;
+    //开始录音
+    private Button startAudio;
+    //结束录音
+    private Button stopAudio;
+    //播放录音
+    private Button playAudio;
+    //删除文件
+    private Button deleteAudio;
+    private ScrollView mScrollView;
+    private TextView tv_audio_succeess;
+    //pcm文件
+    private File file;
+    private String filename;
+    private String local_file;
+    private LinearLayout lnlayout;
+
+
+    private void initView() {
+
+        lnlayout=(LinearLayout)this.findViewById(R.id.reocordlayout);
+        mScrollView = (ScrollView) findViewById(R.id.mScrollView);
+        tv_audio_succeess = (TextView) findViewById(R.id.tv_audio_succeess);
+        printLog("initialze successfully");
+        startAudio = (Button) findViewById(R.id.startAudio);
+        startAudio.setOnClickListener(this);
+        stopAudio = (Button) findViewById(R.id.stopAudio);
+        stopAudio.setOnClickListener(this);
+        playAudio = (Button) findViewById(R.id.playAudio);
+        playAudio.setOnClickListener(this);
+        deleteAudio = (Button) findViewById(R.id.deleteAudio);
+        deleteAudio.setOnClickListener(this);
+    }
+
+    //点击事件
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.startAudio:
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        StartRecord();
+                        Log.e(TAG,"start");
+                    }
+                });
+                thread.start();
+                printLog("start recording");
+                ButtonEnabled(false, true, false);
+                break;
+            case R.id.stopAudio:
+                isRecording = false;
+                ButtonEnabled(true, false, true);
+                printLog("stop recording");
+                break;
+            case R.id.playAudio:
+                PlayRecord();
+                ButtonEnabled(true, false, false);
+                printLog("playing");
+                break;
+            case R.id.deleteAudio:
+                deleFile();
+                break;
+        }
+    }
+
+    //打印log
+    private void printLog(final String resultString) {
+        tv_audio_succeess.post(new Runnable() {
+            @Override
+            public void run() {
+                tv_audio_succeess.append(resultString + "\n");
+                mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+
+    //获取/失去焦点
+    private void ButtonEnabled(boolean start, boolean stop, boolean play) {
+        startAudio.setEnabled(start);
+        stopAudio.setEnabled(stop);
+        playAudio.setEnabled(play);
+    }//开始录音
+    public void StartRecord() {
+        Log.i(TAG,"start recording");//16K采集率
+        int frequency = 16000;//格式
+        int channelConfiguration = AudioFormat.CHANNEL_IN_MONO;//16Bit
+        int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;//生成PCM文件
+//        file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/reverseme.pcm");
+        local_file= Environment.getExternalStorageDirectory().getAbsolutePath()+"/media";
+        file=new File(local_file);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        filename=local_file+"/test.pcm";
+        file=new File(filename);
+        Log.i(TAG,"initialize file");//如果存在，就先删除再创建
+        if (file.exists()){
+            file.delete();
+            Log.i(TAG,"delete file");
+        }
+        try {
+            file.createNewFile();
+            Log.i(TAG,"create new file");
+        } catch (IOException e) {
+            Log.i(TAG,"can not create file");
+            e.toString();
+            throw new IllegalStateException("can not create file" + file.toString());
+        }
+        try {
+            //输出流
+            OutputStream os = new FileOutputStream(file);
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+            int bufferSize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
+            AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, frequency, channelConfiguration, audioEncoding, bufferSize);
+            short[] buffer = new short[bufferSize];
+            audioRecord.startRecording();
+            Log.i(TAG, "start recording");
+            isRecording = true;
+            while (isRecording) {
+                int bufferReadResult = audioRecord.read(buffer, 0, bufferSize);
+                for (int i = 0; i < bufferReadResult; i++) {
+                    dos.writeShort(buffer[i]);
+                }
+            }
+            audioRecord.stop();
+            dos.close();
+        }
+        catch (Throwable t) {
+            Log.e(TAG, "fail to record");
+        }
+    }
+
+    //播放文件
+    public void PlayRecord() {
+        if(file == null){
+            return;
+        }
+        //读取文件
+        int musicLength = (int) (file.length() / 2);
+        short[] music = new short[musicLength];
+        try {
+            InputStream is = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+            int i = 0;
+            while (dis.available() > 0) {
+                music[i] = dis.readShort();
+                i++;
+            }
+            dis.close();
+            AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,16000, AudioFormat.CHANNEL_IN_DEFAULT,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    musicLength * 2,
+                    AudioTrack.MODE_STREAM);
+            audioTrack.play();
+            audioTrack.write(music, 0, musicLength);
+            audioTrack.stop();
+        } catch (Throwable t) {
+            Log.e(TAG, "fail to play");
+        }
+    }
+    //删除文件
+    private void deleFile() {
+        if(file == null){
+            return;
+        }
+        file.delete();
+        printLog("delete file successfully");
+    }
+
+/*
     private void onRecord(boolean start) {
         if (start) {
             startRecording();
@@ -443,4 +642,5 @@ public class MainActivity extends Activity {
             mPlayer = null;
         }
     }
+    */
 }
